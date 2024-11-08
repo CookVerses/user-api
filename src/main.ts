6babf81e805ea277cf06a418041cf439f3fb38be
Cuong3103 <cuongpdse173196@fpt.edu.vn>
2024-11-08 17:11:56 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+/* istanbul ignore file */
+import { NestFactory } from '@nestjs/core';
+import { RequestMethod } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
+import helmet from 'helmet';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
